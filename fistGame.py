953c1ca51004350f0ab29df9042931350892ef57
rumothy <rumothy@yahoo.com>
2021-01-31 02:55:47 -0500

    Image loading comment

--- fistGame.py
+++ fistGame.py
@@ -5,6 +5,34 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("First Game")
 
+# Load an image in the same directory (as firstGame.py): pygame.image.load('R1.png')
+# Load an image in the data folder 1: pygame.image.load(pygame.path.join('data', 'R1.png'))
+# Load an image in the data folder 2: pygame.image.load('data/R1.png')
+
+walkRight = [
+    pygame.image.load('R1.png'),
+    pygame.image.load('R2.png'),
+    pygame.image.load('R3.png'),
+    pygame.image.load('R4.png'),
+    pygame.image.load('R5.png'),
+    pygame.image.load('R6.png'),
+    pygame.image.load('R7.png'),
+    pygame.image.load('R8.png'),
+    pygame.image.load('R9.png'),
+]
+
+walkLeft = [
+    pygame.image.load('L1.png'),
+    pygame.image.load('L2.png'),
+    pygame.image.load('L3.png'),
+    pygame.image.load('L4.png'),
+    pygame.image.load('L5.png'),
+    pygame.image.load('L6.png'),
+    pygame.image.load('L7.png'),
+    pygame.image.load('L8.png'),
+    pygame.image.load('L9.png')
+]
+
 screenWidth = 500
 x = 50
 y = 50
@@ -13,6 +41,9 @@ height = 60
 vel = 10
 isJump = False
 jumpCount = 10
+left = False
+right = False
+walkCount = 0
 
 # main loop
 run = True
