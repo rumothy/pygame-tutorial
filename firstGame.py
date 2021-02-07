e9c01254c325f9d6c46dad2c169e25fdf89a73ec
rumothy <rumothy@yahoo.com>
2021-02-07 17:04:03 -0500

    6 Enemies End

--- firstGame.py
+++ firstGame.py
@@ -83,12 +83,79 @@ class projectile(object):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
+class enemy(object):
+    walkRight = [
+        pygame.image.load('R1E.png'),
+        pygame.image.load('R2E.png'),
+        pygame.image.load('R3E.png'),
+        pygame.image.load('R4E.png'),
+        pygame.image.load('R5E.png'),
+        pygame.image.load('R6E.png'),
+        pygame.image.load('R7E.png'),
+        pygame.image.load('R8E.png'),
+        pygame.image.load('R9E.png'),
+        pygame.image.load('R10E.png'),
+        pygame.image.load('R11E.png'),
+    ]
+    walkLeft = [
+        pygame.image.load('L1E.png'),
+        pygame.image.load('L2E.png'),
+        pygame.image.load('L3E.png'),
+        pygame.image.load('L4E.png'),
+        pygame.image.load('L5E.png'),
+        pygame.image.load('L6E.png'),
+        pygame.image.load('L7E.png'),
+        pygame.image.load('L8E.png'),
+        pygame.image.load('L9E.png'),
+        pygame.image.load('L10E.png'),
+        pygame.image.load('L11E.png'),
+    ]
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.path = [x, end]
+        self.walkCount = 0
+        self.vel = 3
+
+    def draw(self, win):
+        self.move()
+        if self.walkCount + 1 >= 33:
+            self.walkCount = 0
+
+        if self.vel > 0:
+            win.blit(self.walkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(self.walkLeft[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+
+    def move(self):
+        if self.vel > 0:
+            if self.x < self.path[1] + self.vel:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.x += self.vel
+                self.walkCount = 0
+        else:
+            if self.x > self.path[0] - self.vel:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.x += self.vel
+                self.walkCount = 0
+
+
 screenWidth = 500
 
 
 def redrawGameWindow():
     win.blit(bg, (0, 0))
     man.draw(win)
+    goblin.draw(win)
     for bullet in bullets:
         bullet.draw(win)
 
@@ -97,6 +164,7 @@ def redrawGameWindow():
 
 # main loop
 man = player(300, 410, 64, 64)
+goblin = enemy(100, 410, 64, 64, 300)
 bullets = []
 run = True
 while run:
